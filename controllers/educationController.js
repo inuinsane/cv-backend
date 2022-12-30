bfc300c6612e34e0c034f9bed13046286061d8e1
inuinsane <inuinsane@gmail.com>
2022-12-30 11:35:30 +0800

    update education data

--- controllers/educationController.js
+++ controllers/educationController.js
@@ -1,6 +1,7 @@
 import User from "../Models/UserModel.js";
 import { educationValidation } from "../Validations.js";
 
+// create Education Data
 export const createNewEducation = async (req, res) => {
   // Input validation
   if (!req.body)
@@ -31,5 +32,55 @@ export const createNewEducation = async (req, res) => {
     status: "success",
     message: "Perubahan berhasil disimpan",
     data: user.educations,
+  });
+};
+
+// get education data by ID
+export const getEducationData = async (req, res) => {
+  const user = await User.findById(req.params.userId);
+  if (!user)
+    return res.status(404).json({
+      status: "error",
+      message: "Data User tidak ditemukan",
+    });
+
+  if (user.educations.length < 1)
+    return res.status(404).json({
+      status: "error",
+      message: "Data User tidak ditemukan",
+    });
+
+  res.status(404).json({
+    status: "success",
+    message: "Data ditemukan",
+    data: user.educations,
+  });
+};
+
+// update education data by ID
+export const updateEducationData = async (req, res) => {
+  const dataId = req.params.dataId;
+  const selectedData = await User.findOneAndUpdate(
+    {
+      "educations._id": dataId,
+    },
+    {
+      // Harus update secara manual
+      // Apabila update menggunakan req.body, old data akan terhapus
+      // see more https://stackoverflow.com/questions/15691224/mongoose-update-values-in-array-of-objects
+      $set: {
+        "educations.$.schoolName": req.body.schoolName,
+        "educations.$.startYear": req.body.startYear,
+        "educations.$.endYear": req.body.endYear,
+        "educations.$.major": req.body.major,
+        "educations.$.information": req.body.information,
+      },
+    }
+  );
+  const updated = await User.findById(selectedData._id);
+  res.status(201).json({
+    status: "success",
+    message: "Perubahan berhasil disimpan",
+    data: updated.educations,
   });
 };
--- routes/educationRoutes.js
+++ routes/educationRoutes.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { createNewEducation } from "../controllers/educationController.js";
+import {
+  createNewEducation,
+  getEducationData,
+  updateEducationData,
+} from "../controllers/educationController.js";
 import verifyToken from "../VerifyToken.js";
 
 const router = express.Router();
 
-router.patch("/:userId", verifyToken, createNewEducation);
+router.patch("/:userId", verifyToken, createNewEducation);      // create new data
+router.get("/:userId", getEducationData);                       // get data by user ID
+router.patch("/update/:dataId", verifyToken, updateEducationData);     // update data
 export default router;
